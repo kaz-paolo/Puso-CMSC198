11f0ad27686cc19dd598915455a99252351133ce
Ron Paolo Molejona <rqmolejona@up.edu.ph>
2025-10-14 22:28:38 +0800

    updated viteconfig to add pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+    // vite.config.js
+    import { defineConfig } from 'vite';
+    import react from '@vitejs/plugin-react';
+    import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+    export default defineConfig({
+      plugins: [
+        react(),
+        VitePWA({
+
+          // need to add imgs to public folder to enable PWA
+          registerType: 'autoUpdate',
+          includeAssets: ['favicon.svg', 'apple-touch-icon.png', 'masked-icon.png'],
+          manifest: {
+            name: 'My PWA App',
+            short_name: 'MyPWA',
+            theme_color: '#ffffff',
+            icons: [
+              {
+                src: 'pwa-192x192.png',
+                sizes: '192x192',
+                type: 'image/png',
+              },
+              {
+                src: 'pwa-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+              },
+              {
+                src: 'pwa-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'any maskable',
+              },
+            ],
+          },
+        }),
+      ],
+    });
